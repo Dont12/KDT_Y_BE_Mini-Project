b041170ca38b7ed19bdc421e151b5e9459a96b4f
cyPark95 <parkcy1007@gmail.com>
2023-11-22 16:47:01 +0900

    Initialized Project

---
+++ src/main/java/com/fastcampus/reserve/BackendTempApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTempApplication.class, args);
+    }
+
+}
