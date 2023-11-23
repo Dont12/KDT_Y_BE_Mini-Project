005d5c0bd16522a4ca81934da3ccbeea5c660f95
cyPark95 <parkcy1007@gmail.com>
2023-11-23 18:56:41 +0900

    feat: 유효성 검사 utils 클래스 생성

---
+++ src/main/java/com/fastcampus/reserve/common/utils/ValidateUtils.java
@@ -0,0 +1,24 @@
+package com.fastcampus.reserve.common.utils;
+
+import com.fastcampus.reserve.common.exception.CustomException;
+import com.fastcampus.reserve.common.response.ErrorCode;
+import java.util.List;
+import java.util.Objects;
+
+public final class ValidateUtils {
+
+    private ValidateUtils() {
+    }
+
+    public static void nullCheck(Object param) {
+        if (Objects.isNull(param)) {
+            throw new CustomException(ErrorCode.COMMON_INVALID_PARAMETER);
+        }
+    }
+
+    public static void emptyCheck(List<?> param) {
+        if(param.isEmpty()) {
+            throw new CustomException(ErrorCode.COMMON_INVALID_PARAMETER);
+        }
+    }
+}
