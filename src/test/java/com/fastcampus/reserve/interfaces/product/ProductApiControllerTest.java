acadd0c962d8875542118b9f767a12ee5c2bb1e2
deepredk <deepred.kim@gmail.com>
2023-11-26 08:40:57 +0900

    refactor: reformat code 해봤음

--- src/test/java/com/fastcampus/reserve/interfaces/product/ProductApiControllerTest.java
+++ src/test/java/com/fastcampus/reserve/interfaces/product/ProductApiControllerTest.java
@@ -1,7 +1,13 @@
 package com.fastcampus.reserve.interfaces.product;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fastcampus.reserve.application.ProductFacade;
 import com.fastcampus.reserve.interfaces.product.dto.response.ProductResponse;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
@@ -9,18 +15,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import java.util.List;
 import org.springframework.test.web.servlet.MockMvc;
-
-
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class ProductApiControllerTest {
+
     @Autowired
     MockMvc mockMvc;
 
@@ -35,10 +35,10 @@ class ProductApiControllerTest {
     @Test
     void testGetProducts() throws Exception {
         when(productFacade.getProducts())
-                .thenReturn(List.of(new ProductResponse(1L, "name", 0, "imageUrl")));
+            .thenReturn(List.of(new ProductResponse(1L, "name", 0, "imageUrl")));
 
         mockMvc.perform(get("/products"))
-                .andExpect(status().isBadRequest())
-                .andDo(print());
+            .andExpect(status().isBadRequest())
+            .andDo(print());
     }
 }
