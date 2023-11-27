0ac489a10609367c95162cabb39685777eaec25e
Aleexender <alexkjh2011@gmail.com>
2023-11-27 21:32:51 +0900

    Fix: checkStyle 수정

--- src/main/java/com/fastcampus/reserve/infrestructure/product/ProductRepository.java
+++ src/main/java/com/fastcampus/reserve/infrestructure/product/ProductRepository.java
@@ -1,9 +1,8 @@
 package com.fastcampus.reserve.infrestructure.product;
 
 import com.fastcampus.reserve.domain.product.Product;
-import java.util.List;
-
 import com.fastcampus.reserve.domain.product.ProductImage;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
