155bc6f546cce39241d5b7e616da1be12a2cf2e3
cyPark95 <parkcy1007@gmail.com>
2023-11-27 18:38:54 +0900

    docs: 예약 결제 API 문서화

--- src/test/java/com/fastcampus/reserve/restdocs/order/OrderDocumentationTest.java
+++ src/test/java/com/fastcampus/reserve/restdocs/order/OrderDocumentationTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.reserve.restdocs.order;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -13,8 +14,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fastcampus.reserve.common.ApiDocumentation;
+import com.fastcampus.reserve.domain.order.Order;
 import com.fastcampus.reserve.domain.user.User;
 import com.fastcampus.reserve.domain.user.UserReader;
+import com.fastcampus.reserve.infrestructure.order.OrderRepository;
 import jakarta.servlet.http.Cookie;
 import java.util.HashMap;
 import java.util.List;
@@ -22,11 +25,14 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.util.ReflectionTestUtils;
 
 public class OrderDocumentationTest extends ApiDocumentation {
 
     @MockBean
     private UserReader userReader;
+    @MockBean
+    private OrderRepository orderRepository;
 
     @Test
     void registerOrder() throws Exception {
@@ -88,6 +94,56 @@ public class OrderDocumentationTest extends ApiDocumentation {
                                 fieldWithPath("status").ignored(),
                                 fieldWithPath("data.orderToken").type(STRING)
                                         .description("임시 예약 번호")
+                        )
+                ));
+    }
+
+    @Test
+    void payment() throws Exception {
+        Map<String, Object> payment = new HashMap<>();
+        payment.put("orderToken", "58f795f6-2795-4d19-9dba-c1032209c05b");
+        payment.put("userName", "userName");
+        payment.put("userPhone", "userPhone");
+        payment.put("price", 99000);
+        payment.put("payment", "CARD");
+
+        byte[] param = objectMapper.writeValueAsBytes(payment);
+
+        Order order = Order.builder().build();
+        ReflectionTestUtils.setField(order, "id", -1L);
+
+        mockSecuritySetting();
+        when(orderRepository.save(any(Order.class))).thenReturn(order);
+
+        this.mockMvc.perform(
+                        post("/v1/orders/payment")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .cookie(new Cookie("accessToken", "accessToken"))
+                                .content(param)
+                )
+                .andExpect(status().isOk())
+                .andDo(document(
+                        "order/payment/success",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestFields(
+                                fieldWithPath("orderToken").type(STRING)
+                                        .description("임시 예약 신청 번호"),
+                                fieldWithPath("userName").type(STRING)
+                                        .description("이용자 이름"),
+                                fieldWithPath("userPhone").type(STRING)
+                                        .attributes(getPhoneFormat())
+                                        .description("이용자 휴대폰 번호"),
+                                fieldWithPath("price").type(NUMBER)
+                                        .description("예약 가격"),
+                                fieldWithPath("payment").type(STRING)
+                                        .description("결제 수단")
+                        ),
+                        responseFields(
+                                fieldWithPath("status").ignored(),
+                                fieldWithPath("data.orderId").type(NUMBER)
+                                        .description("예약 번호")
                         )
                 ));
     }
