556c6a3804125fbed34c878264fa7acaaa9819cb
Aleexender <alexkjh2011@gmail.com>
2023-11-27 11:36:06 +0900

    fix: mapper 주입

--- src/main/java/com/fastcampus/reserve/application/ProductFacade.java
+++ src/main/java/com/fastcampus/reserve/application/ProductFacade.java
@@ -3,7 +3,9 @@ package com.fastcampus.reserve.application;
 
 import com.fastcampus.reserve.domain.product.Product;
 import com.fastcampus.reserve.domain.product.ProductService;
+import com.fastcampus.reserve.interfaces.product.ProductDtoMapper;
 import com.fastcampus.reserve.interfaces.product.dto.response.ProductResponse;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +17,19 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ProductFacade {
     private final ProductService productService;
+    private final ProductDtoMapper productDtoMapper;
 
-    public List<ProductResponse> getProducts() {
-        List<Product> allProducts = productService.getAllProducts();
+    public List<ProductResponse> getProducts(
+            LocalDate checkIn,
+            LocalDate checkOut,
+            String category,
+            String areaCode,
+            int page,
+            int pageSize) {
+        List<Product> allProducts = productService.getAllProducts(
+                checkIn, checkOut, category, areaCode, page, pageSize);
         return allProducts.stream()
-                .map(ProductResponse::new)
+                .map(productDtoMapper::of)
                 .collect(Collectors.toList());
     }
 
