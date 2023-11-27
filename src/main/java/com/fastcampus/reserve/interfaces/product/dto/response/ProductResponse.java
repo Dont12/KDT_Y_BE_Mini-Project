ab761139cf8b455c33d26054b186da9e7a99d5f5
Aleexender <alexkjh2011@gmail.com>
2023-11-27 11:36:50 +0900

    fix: Builder 패턴으로 변경

--- src/main/java/com/fastcampus/reserve/interfaces/product/dto/response/ProductResponse.java
+++ src/main/java/com/fastcampus/reserve/interfaces/product/dto/response/ProductResponse.java
@@ -2,20 +2,36 @@ package com.fastcampus.reserve.interfaces.product.dto.response;
 
 import com.fastcampus.reserve.domain.product.Product;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
 
-public record ProductResponse(
-        @NotNull Long id,
-        @NotNull String name,
-        Integer minPrice,
-        String imageUrl
-) {
-    public ProductResponse(Product product) {
-        this(
-                product.getId(),
-                product.getName(),
-                product.getMinPrice(),
-                product.getImages().get(0).getUrl()
-        );
+@Getter
+public class ProductResponse {
 
+    @NotNull
+    private final Long id;
+
+    @NotNull
+    private final String name;
+
+    private final Integer minPrice;
+
+    private final String imageUrl;
+
+    @Builder
+    public ProductResponse(Long id, String name, Integer minPrice, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.minPrice = minPrice;
+        this.imageUrl = imageUrl;
+    }
+
+    public static ProductResponse from(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .minPrice(product.getMinPrice())
+                .imageUrl(product.getImages().get(0).getUrl())
+                .build();
     }
 }
