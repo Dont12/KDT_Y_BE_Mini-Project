938427a9205005c2272906583448d40ae85b76f3
Jinhong Kim <deepred.kim@gmail.com>
2023-11-28 09:32:49 +0900

    런타임 오류 해결 (불가능한 Repository 메서드)

--- src/main/java/com/fastcampus/reserve/infrestructure/product/ProductRepository.java
+++ src/main/java/com/fastcampus/reserve/infrestructure/product/ProductRepository.java
@@ -1,15 +1,10 @@
 package com.fastcampus.reserve.infrestructure.product;
 
 import com.fastcampus.reserve.domain.product.Product;
-import com.fastcampus.reserve.domain.product.ProductImage;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllByCategory(String category);
-
-
-    ProductImage findByIdWithImage();
-
 }
