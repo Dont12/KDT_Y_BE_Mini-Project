b7cfd2c86ed4adde8c6cc32f15df392925c939da
Aleexender <alexkjh2011@gmail.com>
2023-11-28 23:12:37 +0900

    fix: 메소드 네이밍 수정

--- src/main/java/com/fastcampus/reserve/infrestructure/product/ProductRepository.java
+++ src/main/java/com/fastcampus/reserve/infrestructure/product/ProductRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllByCategory(String category);
+
+    List<Product> findAllByArea(String area);
 }
