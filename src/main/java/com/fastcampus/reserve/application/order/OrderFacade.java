d7a9c75743ca1255c6600f4ae0deb4c364395767
cyPark95 <parkcy1007@gmail.com>
2023-11-29 10:49:55 +0900

    feat: 주문 내역 조회 작업 정의

--- src/main/java/com/fastcampus/reserve/application/order/OrderFacade.java
+++ src/main/java/com/fastcampus/reserve/application/order/OrderFacade.java
@@ -3,10 +3,13 @@ package com.fastcampus.reserve.application.order;
 import com.fastcampus.reserve.domain.order.OrderService;
 import com.fastcampus.reserve.domain.order.dto.request.PaymentDto;
 import com.fastcampus.reserve.domain.order.dto.request.RegisterOrderDto;
+import com.fastcampus.reserve.domain.order.dto.response.OrderHistoriesDto;
 import com.fastcampus.reserve.domain.order.dto.response.OrderInfoDto;
 import com.fastcampus.reserve.domain.order.dto.response.RegisterOrderInfoDto;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -26,6 +29,10 @@ public class OrderFacade {
 
     public RegisterOrderInfoDto findRegisterOrder(String orderToken) {
         return orderService.findRegisterOrder(orderToken);
+    }
+
+    public List<OrderHistoriesDto> findOrderHistories(Pageable pageable) {
+        return orderService.findOrderHistories(pageable);
     }
 
     public OrderInfoDto findOrder(Long orderId) {
