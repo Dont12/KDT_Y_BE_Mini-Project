64b57908f8cee9888564ec69bec9845f1fd67e62
deepredk <deepred.kim@gmail.com>
2023-11-29 10:27:46 +0900

    fix: Room.producdt EAGER -> LAZY

--- src/main/java/com/fastcampus/reserve/domain/product/room/Room.java
+++ src/main/java/com/fastcampus/reserve/domain/product/room/Room.java
@@ -29,7 +29,7 @@ public class Room extends BaseTimeEntity {
     @Id
     private Long id;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
 
