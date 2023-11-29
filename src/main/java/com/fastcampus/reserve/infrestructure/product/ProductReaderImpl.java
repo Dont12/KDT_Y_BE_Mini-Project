2c51e40af0e9b3e1ebe36e879609b16783ccfbf8
cyPark95 <parkcy1007@gmail.com>
2023-11-29 20:52:25 +0900

    fix: Merge 과정에서 생긴 오류 수정

--- src/main/java/com/fastcampus/reserve/infrestructure/product/ProductReaderImpl.java
+++ src/main/java/com/fastcampus/reserve/infrestructure/product/ProductReaderImpl.java
@@ -3,9 +3,9 @@ package com.fastcampus.reserve.infrestructure.product;
 import static com.fastcampus.reserve.common.response.ErrorCode.NOT_EXIST_PRODUCT;
 
 import com.fastcampus.reserve.common.exception.CustomException;
-import com.fastcampus.reserve.domain.dto.request.ProductListOptionDto;
 import com.fastcampus.reserve.domain.product.Product;
 import com.fastcampus.reserve.domain.product.ProductReader;
+import com.fastcampus.reserve.domain.product.dto.request.ProductListOptionDto;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
