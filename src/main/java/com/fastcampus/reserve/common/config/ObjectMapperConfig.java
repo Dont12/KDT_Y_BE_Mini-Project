0b35949bb3f4e436a7c53df9e03e291833f0112e
Jinhong Kim <deepred.kim@gmail.com>
2023-11-30 19:32:00 +0900

    ㅁㄴㄹ아ㅓㅚ러ㅏ론이ㅓ

--- src/main/java/com/fastcampus/reserve/common/config/ObjectMapperConfig.java
+++ src/main/java/com/fastcampus/reserve/common/config/ObjectMapperConfig.java
@@ -1,6 +1,7 @@
 package com.fastcampus.reserve.common.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -16,6 +17,10 @@ public class ObjectMapperConfig {
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.configure(
+            JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature(),
+            true
+        );
         return objectMapper;
     }
 }
